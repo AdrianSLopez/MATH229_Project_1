354879057dd3935c6cf6f02afd83659772ac14ef
Nep2un <57457579+AdrianSLopez@users.noreply.github.com>
2023-11-12 22:26:08 -0800

    cleaned code + add pie graph for ev make total sales

--- new_zev_sales.py
+++ new_zev_sales.py
@@ -6,51 +6,77 @@ import matplotlib
 from matplotlib import pyplot as plt
 import plotly.express as px
 
-matplotlib.use('MacOSX')
+matplotlib.use('TkAgg')
 from dataCleaning import *
 
 # ELECTRIC VEHICLE DATA
 data_new_zev_sales_LA = Table.read_table('Data/New_ZEV_Sales_Last_updated_10-31-2023_ada_County.csv').where('County', are.equal_to('Los Angeles')).where('FUEL_TYPE', are.equal_to('Electric'))
-data_new_zev_sales_LA_all = data_new_zev_sales_LA.drop('County', 'FUEL_TYPE', 'Make', 'MODEL').group(['Data Year'], sum).where('Data Year', are.above_or_equal_to(2010)).where('Data Year', are.below_or_equal_to(2022)).relabel('Data Year',  'Year').relabel('Number of Vehicles sum', '# of EVs purchased')
-vehicle_make_sales_LA = data_new_zev_sales_LA.drop('County', 'FUEL_TYPE', 'MODEL').where('Data Year', are.above_or_equal_to(2010)).where('Data Year', are.below_or_equal_to(2022)).group(['Data Year', 'MAKE'], sum)
-vehicle_make_sales_LA_tables = createTableForEveryMake(vehicle_make_sales_LA)
+LA_ev_total_sales_per_year_table = data_new_zev_sales_LA.drop('County', 'FUEL_TYPE', 'MAKE', 'MODEL').group(['Data Year'], sum).where('Data Year', are.above_or_equal_to(2010)).where('Data Year', are.below_or_equal_to(2022)).relabel('Data Year',  'Year').relabel('Number of Vehicles sum', 'Total # of new EVs purchased')
+LA_total_ev_sold = Table().with_column('Total EV sold between 2010-22', sum(LA_ev_total_sales_per_year_table.column(1)))
 
-displayHTMLTables(vehicle_make_sales_LA_tables)
+LA_ev_sales_per_year_by_make_table = data_new_zev_sales_LA.drop('County', 'FUEL_TYPE', 'MODEL').where('Data Year', are.above_or_equal_to(2010)).where('Data Year', are.below_or_equal_to(2022)).group(['Data Year', 'MAKE'], sum)
+LA_ev_total_sales_per_year_by_make_tables = createTableForEveryMake(LA_ev_sales_per_year_by_make_table)
+
+LA_ev_total_sold_by_make_tables = [Table().with_columns('Make', table.labels[1], 'Total sold', sum(table.column(1))) for table in LA_ev_total_sales_per_year_by_make_tables]
+LA_ev_total_sold_by_make_table = Table().with_columns('Make', [table.column(0)[0] for table in LA_ev_total_sold_by_make_tables], 'Total sold', [table.column(1)[0] for table in LA_ev_total_sold_by_make_tables]).sort('Total sold', descending=True)
+LA_total_make_sold = Table().with_column('Total sold', sum(LA_ev_total_sold_by_make_table.column(1)))
+
+
+displayHTMLTables([LA_ev_total_sales_per_year_table, LA_total_ev_sold, LA_ev_total_sold_by_make_table, LA_total_make_sold] + LA_ev_total_sales_per_year_by_make_tables)
+
+
 
 # DISPLAY PLOTS USING MacOSX on mac and TkAgg on Windows
 print('displaying plot(s)...')
-# data_new_zev_sales_LA_all.scatter(data_new_zev_sales_LA_all.labels[0], data_new_zev_sales_LA_all.labels[2])
-# print(data_new_zev_sales_LA_all.labels)
-
-
-# display data for every vehicle make
-vehicle_make_sold_graph_1 = px.scatter(vehicle_make_sales_LA_tables[0].to_df(), x="Data Year", y=vehicle_make_sales_LA_tables[0].labels[1], name=vehicle_make_sales_LA_tables[0].labels[1]).update_traces(mode="lines+markers")
-
-for i in range(1, int(len(vehicle_make_sales_LA_tables)/2)):
-    if vehicle_make_sales_LA_tables[i].labels[1] == 'Tesla': continue
-    vehicle_make_sold_graph_1.add_scatter(x=vehicle_make_sales_LA_tables[i].column(0), y=vehicle_make_sales_LA_tables[i].column(1), name=vehicle_make_sales_LA_tables[i].labels[1])
-
-vehicle_make_sold_graph_1.update_layout(
-    title="New cars sold per year",
-    xaxis_title="Data Year",
-    yaxis_title="New Vehicle(s) sold",
-    legend_title="Vehicle makes"
+data_new_zev_sales_LA_all_graph = px.scatter(LA_ev_total_sales_per_year_table.to_df(), x="Year", y=LA_ev_total_sales_per_year_table.labels[1]).update_traces(mode="lines+markers")
+data_new_zev_sales_LA_all_graph.update_layout(
+    title="New electric vehicles (EV) sold per year",
+    xaxis_title="Year",
+    yaxis_title="EV sold"
 )
-vehicle_make_sold_graph_1.show()
+# data_new_zev_sales_LA_all_graph.show()
 
-vehicle_make_sold_graph_2 = px.scatter(vehicle_make_sales_LA_tables[int(len(vehicle_make_sales_LA_tables)/2)].to_df(), x="Data Year", y=vehicle_make_sales_LA_tables[int(len(vehicle_make_sales_LA_tables)/2)].labels[1], name=vehicle_make_sales_LA_tables[int(len(vehicle_make_sales_LA_tables)/2)].labels[1]).update_traces(mode="lines+markers")
+LA_ev_make_sold_rel_freq_pie = px.pie(LA_ev_total_sold_by_make_table.to_df(), values='Total sold', names='Make', title='Electric vehicles sold between 2010-22').update_traces(textposition='inside', textinfo='percent+label').update_layout(uniformtext_minsize=12, uniformtext_mode='hide')
+LA_ev_make_sold_rel_freq_pie.show()
 
-for i in range(int(len(vehicle_make_sales_LA_tables)/2) +1, len(vehicle_make_sales_LA_tables)):
-    if vehicle_make_sales_LA_tables[i].labels[1] == 'Tesla': continue
-    vehicle_make_sold_graph_2.add_scatter(x=vehicle_make_sales_LA_tables[i].column(0), y=vehicle_make_sales_LA_tables[i].column(1), name=vehicle_make_sales_LA_tables[i].labels[1])
+vehicle_make_sold_graph_1 = px.scatter()
+for i in range(0, int(len(LA_ev_total_sales_per_year_by_make_tables)/2)):
+    if LA_ev_total_sales_per_year_by_make_tables[i].labels[1] == 'Tesla': continue
+    vehicle_make_sold_graph_1.add_scatter(x=LA_ev_total_sales_per_year_by_make_tables[i].column(0), y=LA_ev_total_sales_per_year_by_make_tables[i].column(1), name=LA_ev_total_sales_per_year_by_make_tables[i].labels[1])
+    vehicle_make_sold_graph_1.update_layout(
+        title="New electric vehicles (EV) sold per year",
+        xaxis_title="Year",
+        yaxis_title="New Vehicle(s) sold",
+        legend_title="Vehicle makes"
+    )
+# vehicle_make_sold_graph_1.show()
 
-vehicle_make_sold_graph_2.update_layout(
-    title="New cars sold per year",
-    xaxis_title="Data Year",
-    yaxis_title="New Vehicle(s) sold",
-    legend_title="Vehicle makes"
+vehicle_make_sold_graph_2 = px.scatter()
+for i in range(int(len(LA_ev_total_sales_per_year_by_make_tables)/2), len(LA_ev_total_sales_per_year_by_make_tables)):
+    if LA_ev_total_sales_per_year_by_make_tables[i].labels[1] == 'Tesla': continue
+    vehicle_make_sold_graph_2.add_scatter(x=LA_ev_total_sales_per_year_by_make_tables[i].column(0), y=LA_ev_total_sales_per_year_by_make_tables[i].column(1), name=LA_ev_total_sales_per_year_by_make_tables[i].labels[1])
+    vehicle_make_sold_graph_2.update_layout(
+        title="New electric vehicles (EV) sold per year",
+        xaxis_title="Year",
+        yaxis_title="New Vehicle(s) sold",
+        legend_title="Vehicle makes"
+    )
+# vehicle_make_sold_graph_2.show()
+
+
+tesla_sold_graph = px.scatter()
+for table in LA_ev_total_sales_per_year_by_make_tables:
+    if table.labels[1] == 'Tesla':
+        tesla_sold_graph.add_scatter(x=table.column(0), y=table.column(1))
+tesla_sold_graph.update_layout(
+    title="Tesla(s) sold per year",
+    xaxis_title="Year",
+    yaxis_title="Sold",
+    font=dict(
+    family="Courier New, monospace",
+    size=18,
+    color="RebeccaPurple"
+    )
 )
-vehicle_make_sold_graph_2.show()
-# plt.title('New Electric Vehicles purchased per year in Los Angeles 2010-2022 ')
-# plt.grid(color='grey', linestyle = '--', linewidth=0.4)
-# plt.show()
+# tesla_sold_graph.show()
+
